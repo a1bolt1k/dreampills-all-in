df18a199ad8483c4156dd126817c660f5452e548
Egor <stabronnikegor@gmail.com>
2025-06-30 09:35:19 +0300

    Добавления всего проекта

---
+++ src/main.js
@@ -0,0 +1,19 @@
+const fadeElements = document.querySelectorAll('.fade-in-on-scroll');
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('visible');
+        observer.unobserve(entry.target);
+      }
+    });
+  },
+  {
+    threshold: 0.1,
+  }
+);
+
+fadeElements.forEach((element) => {
+  observer.observe(element);
+});
